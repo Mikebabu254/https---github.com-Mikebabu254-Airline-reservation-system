c1328c05de44284640535eff525e83683788c6ed
Michael ndolo <105848178+Mikebabu254@users.noreply.github.com>
2024-10-31 16:03:41 +0300

    adding pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,21 @@ import { useState } from "react";
 import { BroserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <BroserRouter>
+        <Routes>
+          <Route path="/" element={}></Route>
+          <Route path="/Login" element={}></Route>
+          <Route path="/Registration" element={}></Route>
+          <Route path="/Booking" element={}></Route>
+          <Route path="/Profile" element={}></Route>
+          <Route path="/Confirmation" element={}></Route>
+          <Route path="/Flight-details" element={}></Route>
+        </Routes>
+      </BroserRouter>
+    </>
+  );
 }
 
 export default App;
