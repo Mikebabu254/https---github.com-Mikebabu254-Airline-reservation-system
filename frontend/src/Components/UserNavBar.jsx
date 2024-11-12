12f3a3e85569e5862bfec65ab8449832ed7546b2
Eshi <michaelndolo17142@gmail.com>
2024-11-12 18:05:44 +0300

    installing react icon

---
+++ frontend/src/Components/UserNavBar.jsx
@@ -0,0 +1,114 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const UserNavBar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light shadow-sm">
+      <div className="container-fluid">
+        <Link className="navbar-brand" to="/">
+          <img
+            src="/logo.png"  // Add your logo path here
+            alt="Airline Logo"
+            width="60"
+            height="60"
+            className="d-inline-block align-top"
+            style={{ marginRight: '10px' }}
+          />
+          Jet Set AirLine Booking
+        </Link>
+
+        {/* Toggle button for mobile view */}
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        {/* Navbar links */}
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <Link className="nav-link" to="/">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/about">
+                About Us
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/my-bookings">
+                My Bookings
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/check-in">
+                Check-In
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/profile">
+                Profile
+              </Link>
+            </li>
+
+
+            {/* Dropdown menu */}
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link dropdown-toggle"
+                href="#"
+                id="navbarDropdown"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                More
+              </a>
+              <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li>
+                  <Link className="dropdown-item" to="/support">
+                    Customer Support
+                  </Link>
+                </li>
+                <li>
+                  <Link className="dropdown-item" to="/deals">
+                    Deals
+                  </Link>
+                </li>
+                <li>
+                  <Link className="dropdown-item" to="/about">
+                    About Us
+                  </Link>
+                </li>
+                <li><hr className="dropdown-divider" /></li>
+                <li>
+                  <Link className="dropdown-item" to="/logout">
+                    Logout
+                  </Link>
+                </li>
+              </ul>
+            </li>
+
+            {/* Login Button */}
+            <li className="nav-item ms-lg-3">
+              <Link className="btn btn-primary" to="/login">
+                Logout
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default UserNavBar;
--- frontend/src/Pages/HomeUser.jsx
+++ frontend/src/Pages/HomeUser.jsx
@@ -1,10 +1,12 @@
 import react from "react";
 import NavBar from "../Components/NavBar";
 import BookingTicket from "../Components/BookingTicket";
+import UserNavBar from "../Components/UserNavBar";
 
 function HomeUser(){
     return(
         <div>
+            <UserNavBar/>
             <BookingTicket/>
         </div>
     )
