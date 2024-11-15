1bc43ec00cd5884cb93673c5df3643289fee489c
Eshi <michaelndolo17142@gmail.com>
2024-11-15 15:48:59 +0300

    nodemon problems

--- backend/index.js
+++ backend/index.js
@@ -13,8 +13,8 @@ const PORT = 3000;
 
 // Connect to MongoDB
 mongoose.connect("mongodb://127.0.0.1:27017/Jetset-airline-reservation", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+  // useNewUrlParser: true,
+  // useUnifiedTopology: true,
 })
 .then(() => console.log("Connected to MongoDB"))
 .catch(err => console.error("Could not connect to MongoDB", err));
