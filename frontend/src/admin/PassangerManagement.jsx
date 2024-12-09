33396a994f9f8bd4dd8b13df855a443799ec0acd
Eshi <michaelndolo17142@gmail.com>
2024-12-09 17:39:13 +0300

    passanger management

--- frontend/src/admin/PassangerManagement.jsx
+++ frontend/src/admin/PassangerManagement.jsx
@@ -2,7 +2,18 @@ import React from "react";
 
 const PassangerManagement =()=>{
     return(
-        <div>Passanger Management</div>
+        <div className="container mt-4">
+            <h1 className="mb-4 text-center">Passanger Management</h1>
+            <table className="table table-bordered table-striped">
+                <thead><tr>
+                        <th>City Code</th>
+                        <th>City Name</th>
+                        <th>Country Name</th>
+                        <th>Time Zone</th>
+                    </tr>
+                </thead>
+            </table>
+        </div>
     )
 }
 
