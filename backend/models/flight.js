764f29d7af95daa959e18d409fe67a7a1d938a03
Eshi <michaelndolo17142@gmail.com>
2024-12-25 01:34:14 +0300

    date to the database

--- backend/models/flight.js
+++ backend/models/flight.js
@@ -5,7 +5,17 @@ const flightSchema = new mongoose.Schema({
     origin: { type: String, required: true },
     destination: { type: String, required: true },
     time: { type: String, required: true },
-    date: { type: Date, required: true },
+    date: { 
+        type: String, 
+        required: true, 
+        validate: {
+            validator: function (value) {
+                // Check if the value matches YYYY-MM-DD format
+                return /^\d{4}-\d{2}-\d{2}$/.test(value);
+            },
+            message: (props) => `${props.value} is not a valid date format (YYYY-MM-DD).`
+        }
+    },
 });
 
 const FlightModel = mongoose.model("FlightSchedule", flightSchema);
