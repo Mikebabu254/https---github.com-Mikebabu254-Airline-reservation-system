97f105de7eef66bfef9ed9b468437c1c3d591671
Eshiwani <michaelndolo17142@gmail.com>
2025-01-03 01:06:47 +0300

    correcting city router

--- backend/routers/cityRouter.js
+++ backend/routers/cityRouter.js
@@ -3,6 +3,6 @@ const router = express.Router()
 const {addCity, viewCity} = require("../controls/cityControl")
 
 router.post("/add-city", addCity)
-router.get("/cities", viewCity)
+router.get("/get-cities", viewCity)
 
 module.exports = router
