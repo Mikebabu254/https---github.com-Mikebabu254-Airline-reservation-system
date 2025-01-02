eb825f06216c8b536899f1bbf7f34e491baddbf3
Eshiwani <michaelndolo17142@gmail.com>
2025-01-02 11:40:22 +0300

    authenticated users only

--- frontend/src/Pages/Admin.jsx
+++ frontend/src/Pages/Admin.jsx
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import NavBar from "../Components/NavBar";
 import SideNavBar from "../Components/SideNavBar";
+import { useNavigate } from 'react-router-dom';
 
-function Admin(){
-    return(
-        <div>  
-            <SideNavBar/>
-        </div>
-    )
+function Admin() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const isLoggedIn = localStorage.getItem("isLoggedIn");
+    const user = JSON.parse(localStorage.getItem("user"));
+
+    if (!isLoggedIn || !user || user.role !== "admin") {
+      navigate("/login"); 
+    }
+  }, []); 
+
+  return (
+    <div>
+      <SideNavBar /> 
+    </div>
+  );
 }
 
 export default Admin;
--- frontend/src/Pages/HomeUser.jsx
+++ frontend/src/Pages/HomeUser.jsx
@@ -1,11 +1,21 @@
-import react from "react";
-import NavBar from "../Components/NavBar";
 import BookingTicket from "../Components/BookingTicket";
 import UserNavBar from "../Components/UserNavBar";
 import Footer from "../Components/Footer";
 import UserFlightSchedule from "../Components/UserFlightSchedule";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
 function HomeUser(){
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const isLoggedIn = localStorage.getItem("isLoggedIn");
+        const user = JSON.parse(localStorage.getItem("user"));
+    
+        if (!isLoggedIn || !user || user.role !== "admin") {
+          navigate("/login"); 
+        }
+      }, []);
     return(
         <div>
             <UserNavBar/>
