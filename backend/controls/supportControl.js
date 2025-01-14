d49c6c6a3485471914ebd0337689248ec376b0ef
Eshiwani <michaelndolo17142@gmail.com>
2025-01-14 18:30:04 +0300

    Eeeeeiiiiiiii

--- backend/controls/supportControl.js
+++ backend/controls/supportControl.js
@@ -4,10 +4,11 @@ const support = async (req, res)=>{
     const {userID, complain} = req.body
 
     try{
-        const SupportModel = await supportModel.create({
+        supportModel.create({
             userId,
             complaint
-        })
+        });
+        res.status(201).json({message: "complaint successfully submitted. Thank you for t"})
     }catch(Error){
         console.log("An error occur when trying to send data to the database", Error)
     }
